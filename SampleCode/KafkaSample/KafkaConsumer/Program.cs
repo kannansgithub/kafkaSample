a559489d4e34afe76a19b1a1f9ce3a7fda9b1f55
Saranya <saranya.kannan@actlogica.com>
2023-08-12 01:02:44 +0530

    first commit

---
+++ SampleCode/KafkaSample/KafkaConsumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+using System.Text.Json;
+
+var config= new ConsumerConfig
+    {
+    BootstrapServers= "localhost:9092",
+    GroupId="weather_consumer_group",
+    AutoOffsetReset=AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+consumer.Subscribe("wether_topic");
+
+CancellationTokenSource cts = new CancellationTokenSource();
+try
+{
+    while (true)
+    {
+        var response=consumer.Consume(cts.Token);
+        if(response?.Message != null)
+        {
+            var wether = JsonSerializer.Deserialize<Wether>(response.Message.Value);
+            Console.WriteLine($"Received Wether for the City {wether?.State}, with the Temp: {wether?.Temparature}F ");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public record Wether(string State, int Temparature);
---
+++ SampleCode/KafkaSample/KafkaProducer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+using System.Text.Json;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    Console.WriteLine("Producer Started Please enter the state to produce: ");
+
+    string? state;
+    while ((state = Console.ReadLine()) != null)
+    {
+        var response = await producer.ProduceAsync("wether_topic", new Message<Null, string> { Value = JsonSerializer.Serialize(new Wether(state, 70)) });
+        Console.WriteLine(response.Value);
+    }
+}catch(ProduceException<Null,string> ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+public record Wether(string State, int Temparature);
+
